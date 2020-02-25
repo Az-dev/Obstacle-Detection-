501f7a9c88e00fefb77201524e9e12038ced5f9a
Az-dev <aliali.elshazly@gmail.com>
2020-02-25 23:17:52 +0200

    added T1 OVF interrupt

--- interrupt.h
+++ interrupt.h
@@ -2,7 +2,7 @@
 #define INTERRUPT_H_
 /*- FUNCTION-LIKE MACROS ---------------------------------------------------------------------------------------------------*/
 #define sei() __asm__ __volatile__ ("sei" ::: "memory")
-#define cli() __asm__ __volatile__ ("cli":::"memory")
+#define cli() __asm__ __volatile__ ("cli" ::: "memory")
 
 /*ISR_TIMER0_OVF*/
 #define ISR_TIMER0_OVF()\
@@ -13,6 +13,11 @@ void __vector_11(void)
 #define ISR_TIMER0_COMP()\
 void __vector_10(void) __attribute__ ((interrupt));\
 void __vector_10(void)
+
+/*ISR_TIMER1_OVF*/
+#define ISR_TIMER1_OVF()\
+void __vector_9(void) __attribute__ ((interrupt));\
+void __vector_9(void)
 
 /*ISR_TIMER2_OVF*/   
 #define ISR_TIMER2_OVF()\
