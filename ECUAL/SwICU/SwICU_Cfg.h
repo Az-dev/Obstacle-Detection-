0b108069e4edbbd9b6e9a36f7ad4c0aacce79a66
Az-dev <aliali.elshazly@gmail.com>
2020-03-22 12:09:33 +0200

    added SwIcu_Cfg.h

---
+++ ECUAL/SwICU/SwICU_Cfg.h
@@ -0,0 +1,16 @@
+/*
+ * SwICU_Cfg.h
+ *
+ * Created: 3/21/2020 7:31:35 PM
+ *  Author: Az
+ */ 
+
+
+#ifndef SWICU_CFG_H_
+#define SWICU_CFG_H_
+/*- GLOBAL EXTERNS ---------------------------------------------------------------------------------------------------*/
+extern strIcuCfg_t Icu_config;
+
+
+
+#endif /* SWICU_CFG_H_ */
